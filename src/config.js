1b01b3093e7e3eedbdd0d2d962a73e7ab1830acb
MMA-dev-collab <thethreemma@gmail.com>
2026-02-04 20:14:39 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
- export const API_BASE_URL = 'https://backend-0l26gfl1.b4a.run';
+ export const API_BASE_URL = 'https://backend-6ye7.vercel.app';
 
 // Local development URL
 //export const API_BASE_URL = 'http://localhost:4000';
+
 
 
 
