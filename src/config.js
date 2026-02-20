6be5fd65704a60a32cd53192795edf403b2a0d6c
Moataz-AMY <manaatef123@gmail.com>
2026-02-21 00:17:16 +0200

    asda

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-// export const API_BASE_URL = 'https://backend-production-d9a4.up.railway.app';
+ export const API_BASE_URL = 'https://backend-production-d9a4.up.railway.app';
 
 // Local development URL
-export const API_BASE_URL = 'http://localhost:4000';
+//export const API_BASE_URL = 'http://localhost:4000';
 
 
 
