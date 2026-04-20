90767a8954e576aa989cd26462130de74b4a35d1
alfar-programer <mazenalfar01@gmail.com>
2026-04-20 20:55:12 +0200

    fix: stop problem list from rage-quitting on first render 😄
    Resolved async state race causing form to enter submitted mode with empty data

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 
 
-//   export const API_BASE_URL = 'https://backend-omega-self-95.vercel.app';
+  export const API_BASE_URL = 'https://backend-omega-self-95.vercel.app';
 
 
 // Local development URL
- export const API_BASE_URL = 'http://localhost:4000';
+//  export const API_BASE_URL = 'http://localhost:4000';
 
 
 
